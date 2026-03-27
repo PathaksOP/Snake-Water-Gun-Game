0c308f206c4d8282523937c063bf21e4bda627ff
PathaksOP <namitapathak1983@gmail.com>
2026-03-28 00:03:22 +0530

    added highscore system and improved the system

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import random
 import time
+
+level = 0
+
 print('WELCOME TO SNAKE - GUN - WATER! \n'.center(500))
 print('HOW TO PLAY: \n')
 print(
     '- You will have to choose between snake, water & gun\n',
-    '- Te interpreter will choose one of them too.\n',
+    '- The interpreter will choose one of them too.\n',
     '- Between snake & water: snake wins\n',
     '- Between snake & gun: gun wins\n',
     '- Between water & gun: water wins\n',
     '- I both choose the same, there will be a tie.\n'
 )
+
+
+
 names = {
     's' : 'snake',
     'w' : 'water',
@@ -17,20 +23,16 @@ names = {
 }
 def decider(player_choice, computer_choice,names):
     time.sleep(1)
-    if player_choice == 's' and computer_choice == 'w':
+    wins = {
+        's': 'w',
+        'w': 'g',
+        'g': 's'
+    }
+
+    if wins[player_choice] == computer_choice:
         return [True , f'The computer chose: {names[computer_choice]}']
-    if player_choice == 'g' and computer_choice == 's':
-        return [True , f'The computer chose: {names[computer_choice]}']
-    if player_choice == 'w' and computer_choice == 'g':
-        return [True , f'The computer chose: {names[computer_choice]}']
-    if player_choice == 'g' and computer_choice == 'w':
+    else:
         return [False , f'The computer chose: {names[computer_choice]}']
-    if player_choice == 'w' and computer_choice == 's':
-        return [False , f'The computer chose: {names[computer_choice]}']
-    if player_choice == 's' and computer_choice == 'g':
-        return [False , f'The computer chose: {names[computer_choice]}']
-    
-    
 
 
 while True:
@@ -46,6 +48,19 @@ if play_game == 'y':
 
 
     while True:
+        with open('highscore.txt' , 'r') as f:
+            scores = f.readlines()
+
+            if scores:
+                score_list = [int(x) for x in scores]
+                highscore = max(score_list)
+            else:
+                highscore = 0
+
+        
+        
+        print(f'\nHighscore : {highscore}\n')
+       
         while True:
             player_choice = input('\nChoose between snake(S), water(W) & gun(G): ').lower().strip(' ')
 
@@ -80,6 +95,10 @@ if play_game == 'y':
             print(decider(player_choice, computer_choice,names)[1])
             time.sleep(1)
             print('\nYou Win! ')
+            level += 1
+            with open('highscore.txt' , 'a') as f:
+                f.write(str(f'{level}\n'))
+            print(f'Your score is {level} points! ')
             time.sleep(0.25)
             print('\nNext Round!\n')
 
@@ -87,6 +106,7 @@ if play_game == 'y':
             print(decider(player_choice, computer_choice,names)[1])
             time.sleep(1)
             print('\nYou Lose! ')
+            print(f'Your score was {level} points! ')
             time.sleep(0.25)
             while True:
                 play_again = input('\nDo you want to play again? (type "Y" to play and "N" to not play) : ').lower().strip(' ')
@@ -98,9 +118,15 @@ if play_game == 'y':
 
             if play_again == 'y':
                 print('\nLets Play Snake - Gun - Water! \n')
+                with open('highscore.txt' , 'a') as f:
+                    f.write(str(f'{level}\n'))
+                level = 0
             else:
                 print('\nThanks for playing! \n')
+                with open('highscore.txt' , 'a') as f:
+                    f.write(str(f'{level}\n'))
                 break
 
 else:
     print('\nThanks for playing!\n ')
+
